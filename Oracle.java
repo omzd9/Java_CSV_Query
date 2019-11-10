ca59ecab45cfde2b70ba6f2e09d0f60674b8d63f
omzd97 <omzd97@gmail.com>
2019-11-10 23:57:56 +0000

    first Commit read huge file

---
+++ Oracle.java
@@ -0,0 +1,23 @@
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+public class Oracle{
+    
+    static int length = 0x8FFFFFF;  
+ 
+    public static void main(String[] args) throws Exception 
+    {
+        try(RandomAccessFile file = new RandomAccessFile("test.txt", "rw")) 
+        {
+            MappedByteBuffer out = file.getChannel()
+                                        .map(FileChannel.MapMode.READ_WRITE, 0, length);
+             
+            for (int i = 0; i < length; i++) 
+            {
+                out.put((byte) 'x');
+            }
+             
+            System.out.println("Finished writing");
+        }
+    }
+}
